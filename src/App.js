28beed34619a67dbf6b5d1b0a79deff47b1e8e58
chilubagh <ibrahim.dauda@integrify.io>
2021-10-12 14:01:45 +0300

    Added a prop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./App.css";
 
+const Box = (props) => {
+  return <div className="box">Box will be here</div>;
+};
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello Dawood</h1>
-      <p>This is a very simple project</p>
+      <div>
+        <h2>{props.name}</h2>
+        <p>Title:{props.title}</p>
+        <p>Age: {props.age}</p>
+      </div>
+      <header>
+        <h1>Logo</h1>
+      </header>
+      <main>
+        <Box name="Dawood" title="CEO" age="29" />
+        <Box name="Katie" title="Desiner" age="39" />
+        <Box name="Ibra" title="SEO" age="29" />
+      </main>
+      <footer>
+        <p>
+          <small>Copyrights</small>
+        </p>
+      </footer>
     </div>
   );
 }
